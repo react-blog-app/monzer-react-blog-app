8396bafa4d2c7b8762ca375de5aff6985bb29547
react-blog-app <myosif@charlotte.edu>
2026-04-27 00:22:07 -0400

    Refactor routing to use BrowserRouter and new components

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,33 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";
-import BlogPostsPage from "./pages/BlogPostsPage.jsx";
-import SinglePostPage from "./pages/SinglePostPage.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Posts from "./pages/Posts";
+import PostDetails from "./pages/PostDetails";
 
 export default function App() {
   return (
-    <div className="app">
-      <header className="header">
-        <h1>React Blog - Lab 8</h1>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/posts">Blog Posts</Link>
-        </nav>
-      </header>
-
-      <main className="main">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <h2>Welcome</h2>
-                <p>Use the navigation to view blog posts.</p>
-              </div>
-            }
-          />
-          <Route path="/posts" element={<BlogPostsPage />} />
-          <Route path="/posts/:postId" element={<SinglePostPage />} />
-        </Routes>
-      </main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/posts/:postId" element={<PostDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
